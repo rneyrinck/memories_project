4fae2cac66f439c73b77e100df55334ce4537b89
robert Neyrinck <robert.a.neyrinck@gmail.com>
2022-10-22 14:01:22 -0500

    set up mongodb connection after creating new cluster and copying driver code snippet, and setting connection with mongoose.connect in server/index.js

---
+++ server/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose, { mongo } from 'mongoose';
+import cors from 'cors';
+
+const app = express();
+
+app.use(bodyParser.json({limit:"30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit:"30mb", extended: true}));
+app.use(cors());
+
+// https://www.youtube.com/watch?v=ngc9gnGgUdA&list=PL6QREj8te1P7VSwhrMf3D3Xt4V6_SRkhu&index=2&t=0s
+// this will be in .env, mongoDB database connection
+const CONNECTION_URL = "mongodb+srv://rneyrinck:092295@cluster0.8iq4lh0.mongodb.net/?retryWrites=true&w=majority"
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(()=> app.listen(PORT, ()=>console.log(`Server running on port: ${PORT}`)))
+    .catch((error)=>console.log(error.message));
+
+// mongoose.set('useFindAndModify', false);
