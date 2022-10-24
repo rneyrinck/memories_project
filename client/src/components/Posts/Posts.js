86ccbc861678b8e771cb5f5cb86fb631c4d45493
robert Neyrinck <robert.a.neyrinck@gmail.com>
2022-10-24 15:32:38 -0500

    set up post feed

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,22 +1,35 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React from "react";
+import { Grid, CircularProgress } from "@material-ui/core";
+import { useSelector } from "react-redux";
 
-
-import Post from './Post/Post'
+import Post from "./Post/Post";
 import useStyles from "./styles";
 
 const Posts = () => {
-    const posts = useSelector((state)=>state.posts);
-    const classes = useStyles();
+  const posts = useSelector((state) => state.posts);
+  const classes = useStyles();
 
-    console.log(posts)
-    return(
-        <>
-        <h1>Posts</h1>
-        <Post />
-        <Post />
-        </>
+  console.log(posts);
+  return (
+    // if no posts show loading, else show posts
+    !posts.length ? (
+      <CircularProgress />
+    ) : (
+      <Grid
+        className={classes.container}
+        container
+        alignItems="stretch"
+        spacing={3}
+      >
+        {/* iterate over posts and generate them in feed */}
+        {posts.map((post) => (
+          <Grid key={post._id} item xs={12} sm={6}>
+            <Post post={post} />
+          </Grid>
+        ))}
+      </Grid>
     )
-}
+  );
+};
 
 export default Posts;
